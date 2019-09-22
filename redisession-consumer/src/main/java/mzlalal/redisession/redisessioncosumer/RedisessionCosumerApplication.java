f28f0a024a8c7a412f93a57240e40b175fc59580
Mzlalalal <Mzlalalal@users.github.com>
2019-09-22 10:54:48 +0800

    验证登录状态服务

---
+++ redisession-consumer/src/main/java/mzlalal/redisession/redisessioncosumer/RedisessionCosumerApplication.java
@@ -0,0 +1,13 @@
+package mzlalal.redisession.redisessioncosumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisessionCosumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisessionCosumerApplication.class, args);
+	}
+
+}
