5cb8843fa23a2704d762b791b3b6d006954e1702
Mzlalalal <30653078+Mzlalalal@users.noreply.github.com>
2019-09-25 17:02:52 +0800

    合并redis config 类至 redisession-common 中

--- redisession-consumer/src/main/java/mzlalal/redisession/redisessioncosumer/config/RedisConfig.java
+++ redisession-consumer/src/main/java/mzlalal/redisession/redisessioncosumer/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package mzlalal.redisession.redisessioncosumer.config;
 
 import lombok.extern.slf4j.Slf4j;
+import mzlalal.redisession.config.BaseRedisConfig;
 import mzlalal.redisession.redisessioncosumer.listener.RedisKeyExpirationListener;
 import mzlalal.redisession.utils.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,54 +20,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Slf4j
 @Configuration
-public class RedisConfig {
+public class RedisConfig extends BaseRedisConfig {
 
     @Value("${spring.redis.topic}")
     private String redisTopic;
-
-    /**
-     * 实例化 RedisTemplate 对象
-     *
-     * @return
-     */
-    @Bean
-    public RedisTemplate<String, Object> functionDomainRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
-        // 设置序列化 开启事务
-        initDomainRedisTemplate(redisTemplate, redisConnectionFactory);
-        return redisTemplate;
-    }
-
-    /**
-     * 设置数据存入 redis 的序列化方式,并开启事务
-     *
-     * @param redisTemplate
-     * @param factory
-     */
-    private void initDomainRedisTemplate(RedisTemplate<String, Object> redisTemplate, RedisConnectionFactory factory) {
-        //如果不配置Serializer，那么存储的时候缺省使用String，如果用User类型存储，那么会提示错误User can't cast to String！
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-        // 开启事务
-        redisTemplate.setEnableTransactionSupport(true);
-        redisTemplate.setConnectionFactory(factory);
-    }
-
-    /**
-     * 注入封装RedisTemplate
-     *
-     * @return RedisUtil
-     */
-    @Bean(name = "redisUtil")
-    public RedisUtil redisUtil(RedisTemplate<String, Object> redisTemplate) {
-        RedisUtil redisUtil = new RedisUtil();
-        redisUtil.setRedisTemplate(redisTemplate);
-        return redisUtil;
-    }
 
     /**
      * redis信息监听器容器
--- redisession-core/src/main/java/mzlalal/redisession/redisessioncore/config/RedisConfig.java
+++ redisession-core/src/main/java/mzlalal/redisession/redisessioncore/config/RedisConfig.java
@@ -1,12 +1,16 @@
 package mzlalal.redisession.redisessioncore.config;
 
 import lombok.extern.slf4j.Slf4j;
+import mzlalal.redisession.config.BaseRedisConfig;
+import mzlalal.redisession.redisessioncore.listener.RedisKeyExpirationListener;
 import mzlalal.redisession.utils.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,54 +20,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Slf4j
 @Configuration
-public class RedisConfig {
+public class RedisConfig extends BaseRedisConfig {
 
     @Value("${spring.redis.topic}")
     private String redisTopic;
-
-    /**
-     * 实例化 RedisTemplate 对象
-     *
-     * @return
-     */
-    @Bean
-    public RedisTemplate<String, Object> functionDomainRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
-        // 设置序列化 开启事务
-        initDomainRedisTemplate(redisTemplate, redisConnectionFactory);
-        return redisTemplate;
-    }
-
-    /**
-     * 设置数据存入 redis 的序列化方式,并开启事务
-     *
-     * @param redisTemplate
-     * @param factory
-     */
-    private void initDomainRedisTemplate(RedisTemplate<String, Object> redisTemplate, RedisConnectionFactory factory) {
-        //如果不配置Serializer，那么存储的时候缺省使用String，如果用User类型存储，那么会提示错误User can't cast to String！
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-        // 开启事务
-        redisTemplate.setEnableTransactionSupport(true);
-        redisTemplate.setConnectionFactory(factory);
-    }
-
-    /**
-     * 注入封装RedisTemplate
-     *
-     * @return RedisUtil
-     */
-    @Bean(name = "redisUtil")
-    public RedisUtil redisUtil(RedisTemplate<String, Object> redisTemplate) {
-        RedisUtil redisUtil = new RedisUtil();
-        redisUtil.setRedisTemplate(redisTemplate);
-        return redisUtil;
-    }
 
     /**
      * redis信息监听器容器
