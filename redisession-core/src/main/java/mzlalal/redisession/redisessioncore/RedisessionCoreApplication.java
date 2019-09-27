b04192172097ef21f72f87cabb7173a54e279b59
Mzlalalal <30653078+Mzlalalal@users.noreply.github.com>
2019-09-28 00:10:44 +0800

    集成nacos 作为服务发现以及配置中心 可以通过nacos作为配置中心来解决部署项目时候动态修改数据库密码以及加密等

--- redisession-core/src/main/java/mzlalal/redisession/redisessioncore/RedisessionCoreApplication.java
+++ redisession-core/src/main/java/mzlalal/redisession/redisessioncore/RedisessionCoreApplication.java
@@ -2,8 +2,10 @@ package mzlalal.redisession.redisessioncore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RedisessionCoreApplication {
 
 	public static void main(String[] args) {
--- redisession-core/src/main/java/mzlalal/redisession/redisessioncore/controller/LoginController.java
+++ redisession-core/src/main/java/mzlalal/redisession/redisessioncore/controller/LoginController.java
@@ -5,6 +5,8 @@ import mzlalal.redisession.entity.AjaxJson;
 import mzlalal.redisession.constant.GlobalConstant;
 import mzlalal.redisession.utils.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,15 +18,19 @@ import javax.servlet.http.HttpServletResponse;
  * @description: 登录控制器
  * @author: Mzlalal
  */
-@Controller
-@RequestMapping("/sso")
 @Slf4j
+@Controller
+@RefreshScope
+@RequestMapping("/sso")
 public class LoginController {
     /**
      * 获取spring - redis - template 服务
      */
     @Autowired
     RedisUtil redisUtil;
+
+    @Value("${account:admin}")
+    String account;
 
     /**
      * 登录方法
@@ -37,7 +43,6 @@ public class LoginController {
     @ResponseBody
     @RequestMapping("/login")
     AjaxJson login(String userName, String password, String callbackUrl, HttpServletRequest request, HttpServletResponse response) {
-        String account = "admin";
 
         // 返回信息
         AjaxJson aj = new AjaxJson();
