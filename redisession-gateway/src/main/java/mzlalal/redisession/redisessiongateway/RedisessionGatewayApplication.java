49d40fa23b4f593a8c7f9abd3fb50ed49c8bb730
Mzlalalal <Mzlalalal@users.github.com>
2019-10-22 09:01:46 +0800

    新增gateway

---
+++ redisession-gateway/src/main/java/mzlalal/redisession/redisessiongateway/RedisessionGatewayApplication.java
@@ -0,0 +1,13 @@
+package mzlalal.redisession.redisessiongateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisessionGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisessionGatewayApplication.class, args);
+	}
+
+}
---
+++ redisession-gateway/src/test/java/mzlalal/redisession/redisessiongateway/RedisessionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mzlalal.redisession.redisessiongateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisessionGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
