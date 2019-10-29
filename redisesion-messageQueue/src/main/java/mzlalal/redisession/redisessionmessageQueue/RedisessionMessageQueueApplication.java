787550f954006bfa5565691f9babf19abe42cdb0
Mzlalalal <Mzlalalal@users.github.com>
2019-10-29 22:00:41 +0800

    新增gateway 新增消息队列rocketmq

---
+++ redisesion-messageQueue/src/main/java/mzlalal/redisession/redisessionmessageQueue/RedisessionMessageQueueApplication.java
@@ -0,0 +1,13 @@
+package mzlalal.redisession.redisessionmessageQueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisessionMessageQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisessionMessageQueueApplication.class, args);
+	}
+
+}
---
+++ redisesion-messageQueue/src/main/java/mzlalal/redisession/redisessionmessageQueue/controller/MsgController.java
@@ -0,0 +1,15 @@
+package mzlalal.redisession.redisessionmessageQueue.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 消息队列使用
+ */
+@Slf4j
+@RestController
+public class MsgController {
+
+
+
+}
