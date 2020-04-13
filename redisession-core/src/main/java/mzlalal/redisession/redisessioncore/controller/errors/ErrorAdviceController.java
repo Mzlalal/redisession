59c6933e5119ec928eb5e23bc53189db19e0d43d
Mzlalalal <Mzlalalal@users.github.com>
2020-04-13 23:21:00 +0800

    删除重复定义顶级异常处理 修改POM文件

--- redisession-core/src/main/java/mzlalal/redisession/redisessioncore/controller/errors/ErrorAdviceController.java
+++ redisession-core/src/main/java/mzlalal/redisession/redisessioncore/controller/errors/ErrorAdviceController.java
@@ -2,10 +2,11 @@ package mzlalal.redisession.redisessioncore.controller.errors;
 
 import mzlalal.redisession.entity.AjaxJson;
 import mzlalal.redisession.redisessionjwt.exception.JwtException;
-import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * 异常处理
@@ -19,6 +20,7 @@ public class ErrorAdviceController {
      * @return ajaxjson
      */
     @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = JwtException.class)
     public AjaxJson jwtExceptionHandler (JwtException e) {
         AjaxJson aj = new AjaxJson();
@@ -27,18 +29,19 @@ public class ErrorAdviceController {
         return aj;
     }
 
-    /**
-     * redisession - jwt 报错出来的异常
-     * @param e jwt异常
-     * @return ajaxjson
-     */
-    @ResponseBody
-    @ExceptionHandler(value = JwtException.class)
-    public AjaxJson jwtExceptionHandler (Exception e) {
-        AjaxJson aj = new AjaxJson();
-        aj.setErrorCode("9999");
-        aj.setErrorMsg("服务好像出现了未知的异常...");
-        aj.put("errorDetail", ExceptionUtils.getRootCauseMessage(e));
-        return aj;
-    }
+//    /**
+//     * redisession - jwt 报错出来的异常
+//     * @param e jwt异常
+//     * @return ajaxjson
+//     */
+//    @ResponseBody
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(value = JwtException.class)
+//    public AjaxJson jwtExceptionHandler (Exception e) {
+//        AjaxJson aj = new AjaxJson();
+//        aj.setErrorCode("9999");
+//        aj.setErrorMsg("服务好像出现了未知的异常...");
+//        aj.put("errorDetail", ExceptionUtils.getRootCauseMessage(e));
+//        return aj;
+//    }
 }
